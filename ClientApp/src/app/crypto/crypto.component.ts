d4bcb0f6e8dc81943d4b888b1f8d3108854e3527
Yvan Econa <ayecona@hotmail.com>
2018-08-29 22:54:28 -0400

    adding module crypto

--- ClientApp/src/app/crypto/crypto.component.ts
+++ ClientApp/src/app/crypto/crypto.component.ts
@@ -13,3 +13,15 @@ export class CryptoComponent implements OnInit {
   }
 
 }
+
+
+
+//import { Component } from '@angular/core';
+
+///** @title Simple form field */
+//@Component({
+//  selector: 'form-field-overview-example',
+//  templateUrl: 'form-field-overview-example.html',
+//  styleUrls: ['form-field-overview-example.css'],
+//})
+//export class FormFieldOverviewExample { }
